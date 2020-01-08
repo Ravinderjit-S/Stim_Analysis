de9455fa6edee4a1ac46a6b0bd224db27eb36a2d
Ravinderjit Singh <rrr93gt@gmail.com>
2020-01-08 13:55:13 -0500

    Set up A,B,A 3AFC for Binding Behavioral Experiment

--- Stimuli/Binding/Stim_Dev/TestStim_bindABA.m
+++ Stimuli/Binding/Stim_Dev/TestStim_bindABA.m
@@ -1,12 +1,13 @@
 fs = 48828;
-Corr_inds = [09:16];
+Corr_inds = [13:16];
 f_start = 100;
 f_end = 8000; 
 Tones_num = 16;
 ERB_spacing = []; %if specified, takes precedence over Tones_num
-
-[stimA, stimB1, stimB2, envs_A, envs_B1, envs_B2, ERBspace, Tones_f] = Stim_Bind_ABB(Corr_inds, fs, f_start, f_end, Tones_num, ERB_spacing);
-stims = vertcat(stimA,stimB1,stimB2);
+tic()
+[stimA, stimB, stimA2, ~, ~, ~, ERBspace, Tones_f] = Stim_Bind_ABA(Corr_inds, fs, f_start, f_end, Tones_num, ERB_spacing);
+toc()
+stims = vertcat(stimA,stimA2, stimB);
 order = randperm(3);
 
 for i = 1:3
@@ -14,20 +15,20 @@ for i = 1:3
     pause(1.2)
 end
 
-t = 0:1/fs:0.7-1/fs;
+t = 0:1/fs:1.0-1/fs;
 figure
 ax1 = subplot(3,1,1);
 plot(t,stimA), title('A')
 
 ax2 = subplot(3,1,2);
-plot(t,stimB1), title('B1')
+plot(t,stimB), title('B1')
 
 ax3 = subplot(3,1,3);
-plot(t,stimB2), title('B2')
+plot(t,stimA2), title('A2')
 
 linkaxes([ax1,ax2,ax3],'xy')
 ylim([-0.4,0.4])
 
 figure
-spectrogram(stimB1,fs/50,[],[],fs,'yaxis'), ylim([0 10])
+spectrogram(stimB,fs/50,[],[],fs,'yaxis'), ylim([0 10])
 
