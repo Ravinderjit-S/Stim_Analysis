fb5bf9e9219a9f30153e65203f18763865f9730f
Stuffdeveloper <rrr93gt@gmail.com>
2020-03-03 16:20:30 -0500

    bug fix

--- Stimuli/TemporalCoding/Demo_FM_phi.m
+++ Stimuli/TemporalCoding/Demo_FM_phi.m
@@ -168,8 +168,6 @@ for i =1:ntrials*nconds
     
 end
 
-save([subj '_FM_phi.mat'],'FMs','ntrials','respList','correctList','phis','diotic')
-
 Screen('DrawText',PS.window,'Experiment is Over!',PS.rect(3)/2-150,PS.rect(4)/2-25,PS.white);
 Screen('DrawText',PS.window,'Thank You for Your Participation!',PS.rect(3)/2-150,PS.rect(4)/2+100,PS.white);
 Screen('Flip',PS.window);
