ed43c164545c92763059795fe9f8391a7581934b
Ravinderjit Singh <rrr93gt@gmail.com>
2020-03-05 10:25:03 -0500

    made osip stim and moved a folder

--- Analysis/Dynamic Binaural/AnalyzeOSCOR.py
+++ Analysis/Dynamic Binaural/AnalyzeOSCOR.py
@@ -11,11 +11,34 @@ import numpy as np
 import scipy as sp
 import pylab as pl
 import matplotlib.pyplot as plt
-import scipy.io as sio
 import mne
 from anlffr.preproc import find_blinks
+from anlffr.spectral import mtcpca
 from EEGpp import EEGconcatenateFolder
 from mne.preprocessing.ssp import compute_proj_epochs
+import os
+
+
+def ITC(X,TW,fs):
+    """
+    X is time x trial
+    TW is half bandwidth product 
+    """
+    X = X.squeeze()
+    ntaps = 2*TW - 1
+    dpss = sp.signal.windows.dpss(X.shape[0],TW,ntaps)
+    N = int(2**np.ceil(np.log2(X.shape[0])))
+    f = np.arange(0,N)*fs/N
+    itc_taps = np.zeros([N,ntaps])
+    
+    for k in range(0,ntaps):
+        print('tap:',k+1,'/',ntaps)
+        Xf = sp.fft(X*dpss[k,:].reshape(dpss.shape[1],1),axis=0,n=N)
+        itc_taps[:,k] = abs(np.mean(Xf,axis=1) / np.mean(abs(Xf),axis=1))
+
+    itc = itc_taps.mean(axis=1)
+    return itc,f
+
 
 
 nchans = 34;
@@ -24,38 +47,66 @@ refchans = ['EXG1','EXG2']
 data_eeg = [];
 data_evnt = [];
     
-direct_ = '/media/ravinderjit/Data_Drive/EEGdata/OSCOR'
-
+data_loc = '/media/ravinderjit/Data_Drive/Data/EEGdata/DynamicBinaural/OSCOR'
+subject = 'S211_24'
 
 exclude = ['EXG3','EXG4','EXG5','EXG6','EXG7','EXG8']; #don't need these extra external channels that are saved
-#exclude = ['EXG3','EXG4','EXG5','EXG6','EXG7','EXG8',
-#           'A1','A2','A3','A4','A5','A6','A7','A8','A9','A10', 'A11',
-#           'A12','A13','A14','A15','A16','A17','A18','A19','A20','A21','A22', 
-#           'A23','A24','A25','A26','A27','A28','A29','A30','A32']
 
-data_eeg,data_evnt = EEGconcatenateFolder(direct_+'/',nchans,refchans,exclude)
+
+datapath =  os.path.join(data_loc, subject)
+# datapath = '/media/ravinderjit/Data_Drive/Data/EEGdata/EFR'
+data_eeg,data_evnt = EEGconcatenateFolder(datapath+'/',nchans,refchans,exclude)
 data_eeg.filter(l_freq=1,h_freq=300)
 
+#%% Blink Removal
+blinks = find_blinks(data_eeg,ch_name = ['A1'],thresh = 100e-6, l_trans_bandwidth = 0.5, l_freq =1.0)
+blink_epochs = mne.Epochs(data_eeg,blinks,998,tmin=-0.25,tmax=0.25,proj=False,
+                              baseline=(-0.25,0),reject=dict(eeg=500e-6))
+Projs = compute_proj_epochs(blink_epochs,n_grad=0,n_mag=0,n_eeg=8,verbose='DEBUG')
+blink_proj = Projs[0]
 
-epoch_data = mne.Epochs(data_eeg, data_evnt, [1], tmin=-0.1, tmax=1.1,reject=dict(eeg=200e-6)) #, baseline=(-0.2, 0.)) 
+data_eeg.add_proj(blink_proj)
+data_eeg.plot_projs_topomap()
+
+#%% Plot data
+
+epoch_data = mne.Epochs(data_eeg, data_evnt, [1], tmin=-0.2, tmax=1.2,reject=dict(eeg=200e-6), baseline=(-0.2, 0.)) 
 evkd_data = epoch_data.average();
 evkd_data.plot(titles = 'OSCOR')
 
-#data_raw = data_eeg.get_data()[0:31,:]
-data_raw = evkd_data.data[0:31,:]
-data_raw = data_raw.mean(axis=0)
-data_raw = data_raw - data_raw.mean()
+#%% Plot PSD
+data_eeg.plot_psd(fmin=0,fmax=300,tmin=0.0,tmax=1.0,proj=True,average=False)
+
+#%% ITC
+dataAll = epoch_data.get_data(picks = range(0,32))
+data32 = epoch_data.get_data(picks='A32').squeeze().T
+t = epoch_data.times
+t1 = np.where(t>=0)[0][0]
+t2 = np.where(t>=1)[0][0]
+data32 = data32[t1:t2,:]
 fs = data_eeg.info['sfreq']
+TW = 2 #fres = 2*TW
+itc,f = ITC(data32,TW,fs) 
 
-#data_fft = np.fft.fft(data_raw,axis=1)
-#freq = np.fft.fftfreq(data_raw.shape[1],1/fs)
-data_fft = np.fft.fft(data_raw)
-freq = np.fft.fftfreq(data_raw.shape[0],1/fs)
-
-#plt.figure()
-#t = np.arange(0,data_raw.size/fs,1./fs)
-#plt.plot(t,data_raw)
 plt.figure()
-#plt.plot(freq,np.abs(data_fft.T))
-plt.plot(freq,20*np.log10(np.abs(data_fft.T)))
+plt.plot(f,itc)
+
+data_reshaped = np.zeros([dataAll.shape[1],dataAll.shape[0],dataAll.shape[2]]) #ch x trial x time
+for ch in range(0,32):
+    data_reshaped[ch,:,:] = dataAll[:,ch,:]
+    
+params = {}
+params['Fs'] = fs
+params['tapers'] = [TW,2*TW-1]
+params['fpass'] = [1, 100]
+params['itc'] = 1
+
+plv,f = mtcpca(data_reshaped,params)
+plt.figure()
+plt.plot(f,plv)
+plt.xlabel('Freq (hz)')
+
+
+
+
 
---
+++ Stimuli/Binaural_t/OSIP.m
@@ -0,0 +1,32 @@
+function [stim] = OSIP(dur,fs,f,fm,noise)
+%dur = duration in sec
+%fs = sampling rate
+%fm = frequency of OSIP
+%noise = if 1, use white noise
+
+t = 0:1/fs:dur-1/fs;
+
+stimL = sin(2*pi*f.*t);
+stimR = sin(2*pi*f.*t + 2*pi*sin(2*pi*fm.*t));
+stim = [stimL;stimR];
+
+%% make noise with bunch of tones ... kinda wonky
+if noise == 1
+    phases = 2*pi*rand(1,floor(fs/2*dur));
+    freqs = 0:1/dur:floor(fs/2);
+    stimL = zeros(1,dur*fs);
+    stimR = zeros(1,dur*fs);
+    for n = 1:length(phases) 
+        stimL = stimL + sin(2*pi*freqs(n).*t + phases(n));
+        stimR = stimR + sin(2*pi*freqs(n).*t + phases(n) + 2*pi*sin(2*pi*fm.*t));
+    end
+    stim = [stimL/length(phases);stimR/length(phases)];
+end
+
+
+
+    
+
+
+
+
