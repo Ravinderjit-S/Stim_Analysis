b98c72279b4458db89e589094579afe29783695d
Ravinderjit Singh <rrr93gt@gmail.com>
2020-07-11 18:39:15 -0400

    working on a few things

--- Analysis/Neuron Analysis Functions/PLV_nerve.m
+++ Analysis/Neuron Analysis Functions/PLV_nerve.m
@@ -8,6 +8,7 @@ function [plv, f] = PLV_nerve(spikes,stim,fs,TW)
 %stim = vertical vector
 %TW = half time bandwidth product 
 
+
 %% Make Spike Delta waveform (single trial PSTH with 1/fs blocks)
 
 Trials = max(spikes(:,1));
@@ -15,24 +16,24 @@ spk_delta = zeros(length(stim),Trials);
 for k =1:Trials
     spk_times = spikes(spikes(:,1) ==k,2);
     spk_times(spk_times >length(stim)/fs) = []; % ignore spikes when stim off
-    spk_delta(round(spk_times*fs),k) = 1;
+    spk_delta(ceil(spk_times*fs),k) = 1;
 end
 
 %% Calc PLV
 ntaps = 2*TW-1;
 taps = dpss(length(stim),TW,ntaps);
 nfft = 2^ceil(log2(length(stim)));
-plv_taps = zeros(ntaps,nfft);
+plv_taps = zeros(nfft,ntaps);
 
 for m = 1:ntaps
     fprintf(1, 'Taper %d \n', m)
-    Xf = fft(spk_delta.*taps(:,m)',nfft,2);
-    Yf = fft(stim.*taps(:,m)',nfft,2);
+    Xf = fft(spk_delta.*taps(:,m),nfft,1);
+    Yf = fft(stim.*taps(:,m),nfft,1);
     XYf = Xf .* conj(Yf);
-    plv_taps(m,:) = abs(mean(XYf ./ abs(Xmf),1));
+    plv_taps(:,m) = abs(mean(XYf ./ abs(XYf),2));
 end
 
-plv = mean(plv_taps,1);
+plv = mean(plv_taps,2);
 f = 0:fs/nfft:fs-1/nfft; 
 
 
--- Stimuli/Binding/Stim_Dev/TestStim_bindAB1AB2.m
+++ Stimuli/Binding/Stim_Dev/TestStim_bindAB1AB2.m
@@ -1,7 +1,7 @@
 fs = 48828;
 %Corr_inds = [12:16];
-Corr_inds1 = [5:16];
-Corr_inds2 = [1:12];
+Corr_inds1 = [];
+Corr_inds2 = [1:8];
 f_start = 100;
 f_end = 8000; 
 Tones_num = 16;
