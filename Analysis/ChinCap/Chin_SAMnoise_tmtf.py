b912a718983c69ba1a38c63536a974e9824c3c3c
Ravinderjit-S <rrr93gt@gmail.com>
2021-03-10 21:45:44 -0600

    Fixed Bugs

--- Analysis/ChinCap/Chin_SAMnoise_tmtf.py
+++ Analysis/ChinCap/Chin_SAMnoise_tmtf.py
@@ -17,21 +17,22 @@ import scipy.io as sio
 
 
 folder = 'Chin_SAM_tmtf/'
-data_loc = '/media/ravinderjit/Storage2/ChinCap/'
-#data_loc = '/home/ravinderjit/Documents/ChinCapData/'
+#data_loc = '/media/ravinderjit/Storage2/ChinCap/'
+data_loc = '/home/ravinderjit/Documents/ChinCapData/'
 nchans = 35
 # refchans = ['A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12','A13','A14','A15','A16','A17','A18','A19',
 #             'A20','A21','A22','A23','A29','A30','A31','A32']
 refchans = ['EXG1','EXG2']
 exclude = ['EXG4','EXG5','EXG6','EXG7','EXG8']
 data_eeg,evnts_eeg = EEGconcatenateFolder(data_loc + folder ,nchans,refchans,exclude)
-data_eeg.filter(1,3500) 
+data_eeg.filter(5,3500) 
 data_eeg.set_channel_types({'EXG3':'eeg'})
 
 AMf = [20,30,40,55,70,90,110,170,250,400,600,800,1000,3000]
-bad_chs = ['A1','A25','A26','A27','A28','EXG3']#,'EXG1','EXG2','A20']
-data_eeg.drop_channels(bad_chs)
-#data_eeg.set_eeg_reference(ref_channels='average')
+bad_chs = ['A1','A25','A26','A27','A28','EXG3','EXG1','EXG2','A20','A21','A22','A30']
+data_eeg.info['bads'] = bad_chs
+#data_eeg.drop_channels(bad_chs)
+data_eeg.set_eeg_reference(ref_channels='average')
 
 scalings = dict(eeg=20e-6,stim=1)
 data_eeg.plot(events = evnts_eeg, scalings=scalings,show_options=True)
@@ -45,10 +46,10 @@ for m in np.arange(len(AMf)):
     
 
 
-Aud_picks = ['A30', 'A6', 'A29', 'A7', 'A4', 'A17', 'A32', 'A10', 'A3']
-All_picks = ['A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10',
- 'A11', 'A12', 'A13', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19',
- 'A21', 'A22', 'A23', 'A24', 'A29', 'A30', 'A31', 'A32']  #Took out A20
+# Aud_picks = ['A30', 'A6', 'A29', 'A7', 'A4', 'A17', 'A32', 'A10', 'A3']
+# All_picks = ['A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10',
+#  'A11', 'A12', 'A13', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19',
+#  'A21', 'A22', 'A23', 'A24', 'A29', 'A30', 'A31', 'A32']  #Took out A20
 
 t = epochs_all[0].times
 t1 = np.where(t>=0)[0][0]
@@ -57,7 +58,7 @@ t = t[t1:t2]
 
 epoch_dat = []  
 for m in range(len(AMf)):
-    epoch_dat_m = epochs_all[m].get_data(picks=All_picks)
+    epoch_dat_m = epochs_all[m].get_data(picks='data')
     epoch_dat_m = epoch_dat_m[:,:,t1:t2].transpose(1,0,2)
     epoch_dat.append(epoch_dat_m)
 
@@ -89,29 +90,32 @@ for m in np.arange(len(AMf)):
 AMlabels = []
 for AM in AMf: AMlabels.append(str(AM))
 plt.figure()
-plt.plot(np.log2(AMf),tmtf_plv.T)
-plt.xticks(ticks=np.log2(AMf),labels=AMlabels)
+plt.semilogx(AMf,tmtf_plv.T)
+plt.xticks(ticks=AMf,labels=AMlabels)
 plt.title('TMTF_PLV')
-plt.xlim((0,1000))
+#plt.xlim((0,1000))
     
 
 fs = epochs_all[0].info['sfreq']
-nfft = 2**np.ceil(np.log2(epoch_dat[0].shape[1]))
-MaxAud_pxx = np.zeros((epoch_dat[0].shape[0],len(AMf)))
-tmtf_pxx = np.zeros((epoch_dat[0].shape[0],len(AMf)))
+nfft = int(2**np.ceil(np.log2(epoch_dat[0].shape[2])))
+MaxAud_Axx = np.zeros((epoch_dat[0].shape[0],len(AMf)))
+tmtf_Axx = np.zeros((epoch_dat[0].shape[0],len(AMf)))
+f = np.arange(0,fs,fs/nfft)
 for m in np.arange(len(AMf)):
-    f, Pxx = periodogram(epoch_dat[m].mean(axis=2),fs,nfft=nfft)
+    Axx = np.fft.fft(epoch_dat[m].mean(axis=1),n=nfft)
+    Axx = np.abs(Axx)
     plt.figure()
-    floc1 = np.where(f>=AMf[m]-40)[0][0]
-    floc2 = np.where(f>=AMf[m]+40)[0][0]
-    MaxAud_pxx[:,m] = np.flip(np.argsort(np.max(Pxx[:,floc1:floc2],axis=1)))
-    tmtf_pxx[:,m] = np.max(Pxx[:,floc1:floc2],axis=1)
-    plt.plot(f,Pxx.T)
+    floc1 = np.where(f>=AMf[m]-20)[0][0]
+    floc2 = np.where(f>=AMf[m]+20)[0][0]
+    MaxAud_Axx[:,m] = np.flip(np.argsort(np.max(Axx[:,floc1:floc2],axis=1)))
+    tmtf_Axx[:,m] = np.max(Axx[:,floc1:floc2],axis=1)
+    plt.plot(f,Axx.T)
+    plt.xlim([0,4000])
     plt.title(str(AMf[m]))
     
 plt.figure()
-plt.plot(np.log2(AMf),tmtf_pxx.T)
-plt.xticks(ticks=np.log2(AMf),labels=AMlabels)
+plt.semilogx(AMf,tmtf_Axx.T)
+plt.xticks(AMf,labels=AMlabels)
 plt.title('TMTF_pxx')
 plt.xlim((0,1000))
     
