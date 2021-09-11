6366f7a94d82d2ff1bd16dfb8973e33b7ced33dc
Ravinderjit Singh <rrr93gt@gmail.com>
2021-09-11 17:57:13 -0400

    Dyn Bin code update

--- Analysis/Dynamic Binaural/EEG_BinMseq/DynBin_sysFuncs_averageSubjects_PCA.py
+++ Analysis/Dynamic Binaural/EEG_BinMseq/DynBin_sysFuncs_averageSubjects_PCA.py
@@ -27,8 +27,6 @@ fig_path = os.path.abspath('/media/ravinderjit/Data_Drive/Data/Figures/DynBin/')
 
 
 Subjects = ['S001','S132','S203','S204','S205','S206','S207','S208','S211']
-Subjects = ['S001','S132','S203','S204','S205','S206','S207','S208','S211']
-
 
 A_IAC_Ht = []
 A_ITD_Ht = []
@@ -57,31 +55,11 @@ for sub in range(len(Subjects)):
         [t, IAC_Ht, ITD_Ht, IAC_Htnf, ITD_Htnf,Tot_trials_IAC,Tot_trials_ITD] = pickle.load(file)
         
         print(sub)
-        
-        # [t,IAC_Ht,ITD_Ht,IAC_Htnf,ITD_Htnf,
-        #     pca_space_IAC,pca_coeff_IAC,pca_expVar_IAC,
-        #     pca_space_ITD,pca_coeff_ITD,pca_expVar_ITD, 
-        #     pca_space_IAC_nf,pca_coeffs_IAC_nf,
-        #     pca_expVar_IAC_nf, pca_space_ITD_nf,
-        #     pca_coeffs_ITD_nf,pca_expVar_ITD_nf, ica_space_ITD,
-        #     ica_coeff_ITD, ica_space_IAC, ica_coeff_IAC, 
-        #     ica_space_ITD_nf,ica_f_ITD_nf,ica_coeffs_ITD_nf,
-        #     ica_space_IAC_nf,ica_coeffs_IAC_nf]= pickle.load(file)
-        
-              # [t,f,IAC_Ht,ITD_Ht,IAC_Htnf,ITD_Htnf,IAC_Hf,ITD_Hf,
-              #   pca_space_IAC,pca_f_IAC,pca_coeff_IAC,pca_expVar_IAC,
-              #   pca_space_ITD,pca_f_ITD,pca_coeff_ITD,pca_expVar_ITD, 
-              #   pca_space_IAC_nf,pca_f_IAC_nf,pca_coeffs_IAC_nf,
-              #   pca_expVar_IAC_nf, pca_space_ITD_nf,pca_f_ITD_nf,
-              #   pca_coeffs_ITD_nf,pca_expVar_ITD_nf, ica_space_ITD,
-              #   ica_f_ITD,ica_coeff_ITD, ica_space_IAC, ica_f_IAC, 
-              #   ica_coeff_IAC, ica_space_ITD_nf,ica_f_ITD_nf,ica_coeffs_ITD_nf,
-              #   ica_space_IAC_nf, ica_f_IAC_nf,ica_coeffs_IAC_nf] = pickle.load(file)
 
     A_IAC_Ht.append(IAC_Ht)
     A_ITD_Ht.append(ITD_Ht)
-    A_IAC_Ht_nf.append(IAC_Htnf[:10])
-    A_ITD_Ht_nf.append(ITD_Htnf[:10])
+    A_IAC_Ht_nf.append(IAC_Htnf)
+    A_ITD_Ht_nf.append(ITD_Htnf)
     
              
 
