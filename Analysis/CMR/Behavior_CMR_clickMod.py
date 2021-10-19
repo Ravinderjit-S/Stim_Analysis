a4c1c697c9079ec934bbf195a04d2ca7f7f61e69
Ravinderjit Singh <rrr93gt@gmail.com>
2021-10-19 17:15:10 -0400

    CMR and Binding analysis

--- Analysis/CMR/Behavior_CMR_clickMod.py
+++ Analysis/CMR/Behavior_CMR_clickMod.py
@@ -15,73 +15,60 @@ from scipy.special import erf
 import psignifit as ps
 
 
-def GaussCDF(x,sigma,mu,lapseRate):
-    return 0.5 *(1+erf((x-mu)/(sigma * np.sqrt(2)))) * (1-1/3-lapseRate)  + 1/3  # 1/3 for 3 AFC
+data_loc = '/media/ravinderjit/Data_Drive/Data/MTB_Behavior/CMR_beh/'
 
-data_loc = '/media/ravinderjit/Data_Drive/Data/BehaviorData/CMR/'
+Subjects = [ 'S259', 'S268', 'S269', 'S270', 'S271', 'S272', #Add S211, S246 later
+ 'S273', 'S274', 'S277', 'S279', 'S280', 'S282', 'S284', 'S285'] 
 
-Subjects = ['S211','SVM', 'S211_coh1']
-subject = Subjects[0]
-data = sio.loadmat(data_loc + subject + '_CMRrandModClicky.mat',squeeze_me=True)
-ntrials = 20
+CMR = np.zeros((len(Subjects)))
 
-SNRs_0 = np.concatenate((np.array([-10]),np.arange(-25,-55,-5)))
-SNRs_1 = np.concatenate((np.array([-15]),np.arange(-35,-65,-5)))
+for sub in range(len(Subjects)):
 
-# SNRs_0 = data['SNR_0']
-# SNRs_1 = data['SNR_1']
-
-snrList_unique = np.concatenate((SNRs_0,SNRs_1))
-snrList = matlib.repmat(snrList_unique,1,20).squeeze()
-
-cohList = data['cohList']
-correctList = data['correctList']
-respList = data['respList']
-
-acc_0 = np.zeros([SNRs_0.size])
-acc_1 = np.zeros([SNRs_1.size])
-
-for ii in range(acc_0.size):
-    mask = (cohList == 0) & (snrList == SNRs_0[ii])
-    acc_0[ii] = np.sum(correctList[mask] == respList[mask]) / ntrials
+    subject = Subjects[sub]
+    data = sio.loadmat(data_loc + subject + '_CMRrandModClicky.mat',squeeze_me=True)
+    ntrials = 20
     
-for ii in range(acc_1.size):
-    mask = (cohList ==1) & (snrList == SNRs_1[ii])
-    acc_1[ii] = np.sum(correctList[mask] == respList[mask]) / ntrials
+    SNRs_0 = np.concatenate((np.array([-10]),np.arange(-25,-55,-5)))
+    SNRs_1 = np.concatenate((np.array([-20]),np.arange(-37,-61,-3)))
     
+    snrList_unique = np.concatenate((SNRs_0,SNRs_1))
+    snrList = matlib.repmat(snrList_unique,1,20).squeeze()
     
-param_0, pcov_0 = curve_fit(GaussCDF,SNRs_0,acc_0,[2,-40,1-acc_0[0]])
-x_0 = np.arange(SNRs_0[-1],SNRs_0[0],.1)
-psy_curve_0 = GaussCDF(x_0,param_0[0],param_0[1],param_0[2])
-
-param_1, pcov_1 = curve_fit(GaussCDF,SNRs_1,acc_1,[2,-50,1-acc_1[0]])
-x_1 = np.arange(SNRs_1[-1],SNRs_1[0],.1)
-psy_curve_1 = GaussCDF(x_1,param_1[0],param_1[1],param_1[2])
+    cohList = data['cohList']
+    correctList = data['correctList']
+    respList = data['respList']
     
-plt.figure()
-plt.plot(SNRs_0, acc_0)     
-plt.plot(SNRs_1, acc_1)
-plt.plot(x_0,psy_curve_0,'--')   
-plt.plot(x_1,psy_curve_1,'--')
+    acc_0 = np.zeros([SNRs_0.size])
+    acc_1 = np.zeros([SNRs_1.size])
+    
+    for ii in range(acc_0.size):
+        mask = (cohList == 0) & (snrList == SNRs_0[ii])
+        acc_0[ii] = np.sum(correctList[mask] == respList[mask]) / ntrials
+        
+    for ii in range(acc_1.size):
+        mask = (cohList ==1) & (snrList == SNRs_1[ii])
+        acc_1[ii] = np.sum(correctList[mask] == respList[mask]) / ntrials
 
-
-#%% Do fit with psignifit
-
-data_0ps = np.concatenate((SNRs_0[:,np.newaxis], acc_0[:,np.newaxis] * ntrials, 20*np.ones([7,1])),axis=1)
-data_1ps = np.concatenate((SNRs_1[:,np.newaxis], acc_1[:,np.newaxis] * ntrials, 20*np.ones([acc_1.size,1])),axis=1)
-
-options = dict({
-    'sigmoidName': 'norm',
-    'expType': '3AFC'
-    })
-
-result_0ps = ps.psignifit(data_0ps, options)
-result_1ps = ps.psignifit(data_1ps, options)
-
-plt.figure()
-ps.psigniplot.plotPsych(result_0ps)
-ps.psigniplot.plotPsych(result_1ps, dataColor='tab:orange',lineColor='tab:orange')
-
-
-
+    #%% Do fit with psignifit
+    
+    data_0ps = np.concatenate((SNRs_0[:,np.newaxis], acc_0[:,np.newaxis] * ntrials, 20*np.ones([7,1])),axis=1)
+    data_1ps = np.concatenate((SNRs_1[:,np.newaxis], acc_1[:,np.newaxis] * ntrials, 20*np.ones([acc_1.size,1])),axis=1)
+    
+    options = dict({
+        'sigmoidName': 'norm',
+        'expType': '3AFC'
+        })
+    
+    result_0ps = ps.psignifit(data_0ps, options)
+    result_1ps = ps.psignifit(data_1ps, options)
+    
+    plt.figure()
+    ps.psigniplot.plotPsych(result_0ps)
+    ps.psigniplot.plotPsych(result_1ps, dataColor='tab:orange',lineColor='tab:orange')
+    plt.title(Subjects[sub])
+    
+    percentCorr = 0.75
+    CMR[sub] = ps.getThreshold(result_0ps,percentCorr)[0] - ps.getThreshold(result_1ps,percentCorr)[0]
+    
+sio.savemat(data_loc + 'CMRclickMod.mat',{'CMR':CMR, 'Subjects':Subjects})
 
---
+++ Analysis/MTB_proj/prelimAnalysis.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 16:38:12 2021
+
+@author: ravinderjit
+"""
+
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+
+data_loc_binding = '/media/ravinderjit/Data_Drive/Data/MTB_Behavior/MTB_beh/'
+data_loc_cmr = '/media/ravinderjit/Data_Drive/Data/MTB_Behavior/CMR_beh/'
+
+beh_bind = sio.loadmat(data_loc_binding + 'BindingBeh.mat',squeeze_me=True)
+beh_cmr = sio.loadmat(data_loc_cmr + 'CMRclickMod.mat',squeeze_me=True)
+
+
+#%% Get Subjects in right order for Data
+#CMR and Binding don't necessarily have Subjects in same order or have the same
+#Subjects, so need to account for that. Only analyze Subjects who have done 
+#both tasks
+
+Subj_CMR = list(beh_cmr['Subjects'])
+Subj_bind = list(beh_bind['Subjects'])
+
+Subjects = list(set(Subj_CMR) & set(Subj_bind)) #Subjects that have done CMR and Binding
+Subjects.sort()
+
+cmr_sort = np.zeros(len(Subjects),dtype=int)
+bind_sort = np.zeros(len(Subjects),dtype=int)
+
+for s in range(len(Subjects)):
+    cmr_sort[s] = Subj_CMR.index(Subjects[s])
+    bind_sort[s] = Subj_bind.index(Subjects[s])
+
+acc_bind = beh_bind['acc']
+cmr = beh_cmr['CMR']
+
+cmr = cmr[cmr_sort]
+acc_bind = acc_bind[:,bind_sort]
+
+#%% Plot simple relationships
+
+# For binding:
+# 0 = 19:20
+# 1 = 17-20
+# 2 = 15-20
+# 3 = 13:20
+# 4 = 1, 8, 14, 20
+# 5 = 1, 4, 8, 12, 16, 20
+# 6 = 1, 4, 6, 9, 12, 15, 17, 20
+
+plt.figure()
+plt.scatter(cmr,acc_bind[1])
+plt.xlabel('CMR (dB)')
+plt.ylabel('Accurary')
+plt.ylim([0.3,1])
+plt.title('Binding 4 cont')
+
+plt.figure()
+plt.scatter(cmr,acc_bind[2])
+plt.xlabel('CMR (dB)')
+plt.ylabel('Accurary')
+plt.ylim([0.3,1])
+plt.title('Binding 6 cont')
+
+
+plt.figure()
+plt.scatter(cmr,acc_bind[5])
+plt.xlabel('CMR (dB)')
+plt.ylabel('Accurary')
+plt.ylim([0.3,1])
+plt.title('Binding 6 spaced')
+
+
+
+
+
+
+
